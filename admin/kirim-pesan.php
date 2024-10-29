4cc43bea1d7030488c1100741b6399952c55dbd2
nasywasyifa <nasywahanifahnasy@gmail.com>
2024-10-29 15:47:43 +0700

    first commit porto

---
+++ admin/kirim-pesan.php
@@ -0,0 +1,215 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+// Jika button simpan di tekan
+if (isset($_GET['pesanId'])) {
+    $id = intval($_GET['pesanId']); // Menggunakan intval untuk menghindari injeksi SQL
+    $selectContact = mysqli_query($koneksi, "SELECT * FROM contact WHERE id = $id");
+    $rowContact = mysqli_fetch_assoc($selectContact);
+}
+
+if (isset($_POST['kirim-bosss']) && isset($_GET['pesanId'])) {
+    $id = $_GET['pesanId']; // Pastikan ID adalah integer
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL); // Sanitasi email
+    $subject = htmlspecialchars($_POST['subject']); // Menghindari XSS
+    $balaspesan = htmlspecialchars($_POST['balaspesan']); // Menghindari XSS
+
+    $headers = "From: nasywahanifahnasy@gmail.com" . "\r\n" .
+        "Reply-To: nasywahanifahnasy@gmail.com" . "\r\n" .
+        "Content-Type: text/plain; charset=UTF-8" . "\r\n" .
+        "MIME-Version: 1.0" . "\r\n";
+
+    if (mail($email, $subject, $balaspesan, $headers)) {
+        header("Location: contact-admin.php?status=berhasil-terkirim");
+        exit();
+    } else {
+        header("Location: kirim-pesan.php?status=gagal-terkirim");
+        exit();
+    }
+}
+
+?>
+
+
+<!DOCTYPE html>
+
+<!-- =========================================================
+* Sneat - Bootstrap 5 HTML Admin Template - Pro | v1.0.0
+==============================================================
+
+* Product Page: https://themeselection.com/products/sneat-bootstrap-html-admin-template/
+* Created by: ThemeSelection
+* License: You must have a valid license purchased in order to legally use the theme for your project.
+* Copyright ThemeSelection (https://themeselection.com)
+
+=========================================================
+ -->
+<!-- beautify ignore:start -->
+<html
+    lang="en"
+    class="light-style layout-menu-fixed"
+    dir="ltr"
+    data-theme="theme-default"
+    data-assets-path="../assets/"
+    data-template="vertical-menu-template-free">
+
+<head>
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <title>Dashboard - Analytics | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+
+    <meta name="description" content="" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="../assets/img/favicon/favicon.ico" />
+
+    <?php include 'inc/head.php' ?>
+</head>
+
+<body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <!-- Menu -->
+
+            <?php include 'inc/sidebar.php'; ?>
+            <!-- / Menu -->
+
+            <!-- Layout container -->
+            <div class="layout-page">
+                <!-- Navbar -->
+
+                <?php include 'inc/nav.php'; ?>
+
+                <!-- / Navbar -->
+
+                <!-- Content wrapper -->
+                <div class="content-wrapper">
+                    <!-- Content -->
+
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="card">
+                                    <div class="card-header">Balas Pesan</div>
+                                    <div class="card-body">
+                                        <ul style="list-style-type: '-'">
+                                            <li>
+                                                <pre>Name : <?php echo $rowContact['name'] ?></pre>
+                                            </li>
+                                            <li>
+                                                <pre>Email : <?php echo $rowContact['email'] ?></pre>
+                                            </li>
+                                            <li>
+                                                <pre>Subject : <?php echo $rowContact['subject'] ?></pre>
+                                            </li>
+                                            <li>
+                                                <pre>Message : <?php echo $rowContact['message'] ?></pre>
+                                            </li>
+                                        </ul>
+                                        <?php
+
+                                        // if (isset($_GET['hapus'])): 
+                                        ?>
+                                        <!-- <div class="alert alert-success" role="alert"> -->
+                                        <!-- Data berhasil dihapus</div> -->
+
+
+                                        <form action="" method="post" enctype="multipart/form-data">
+                                            <div>
+                                                <input type="text" name="email" value="<?php echo $rowContact['email'] ?>">
+                                            </div>
+                                            <mt class="mt-3">
+                                                <label class="form-label" for="">Subject</label>
+                                                <input type="text" class="form-control" name="subject" required>
+                                    </div>
+                                    <div class="mt-3">
+                                        <label for="" class="form-label">Balas Pesan</label>
+                                        <textarea class="form-control" name="balaspesan" cols="30" rows="10" required id=""></textarea>
+                                    </div>
+                                    <div class="mt-3">
+                                        <button class="btn btn-primary" name="kirim-bosss"> Kirim Pesan</button>
+                                    </div>
+                                    </form>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </div>
+            <!-- / Content -->
+
+            <!-- Footer -->
+            <footer class="content-footer footer bg-footer-theme">
+                <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+                    <div class="mb-2 mb-md-0">
+                        ©
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script>
+                        , made with ❤️ by
+                        <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+                    </div>
+                    <div>
+                        <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
+                        <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
+
+                        <a
+                            href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/"
+                            target="_blank"
+                            class="footer-link me-4">Documentation</a>
+
+                        <a
+                            href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
+                            target="_blank"
+                            class="footer-link me-4">Support</a>
+                    </div>
+                </div>
+            </footer>
+            <!-- / Footer -->
+
+            <div class="content-backdrop fade"></div>
+        </div>
+        <!-- Content wrapper -->
+    </div>
+    <!-- / Layout page -->
+    </div>
+
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
+
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="../assets/admin/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/admin/assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/admin/assets/vendor/js/bootstrap.js"></script>
+    <script src="../assets/admin/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="../assets/admin/assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="../assets/admin/assets/vendor/libs/apex-charts/apexcharts.js"></script>
+
+    <!-- Main JS -->
+    <script src="../assets/admin/assets/js/main.js"></script>
+
+    <!-- Page JS -->
+    <script src="../assets/admin/assets/js/dashboards-analytics.js"></script>
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,160 @@
+<?php
+include 'admin/koneksi.php';
+$queryKonten = mysqli_query($koneksi, "SELECT * FROM konten");
+$rowKonten = mysqli_fetch_assoc($queryKonten);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Index - Personal Bootstrap Template</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+
+  <!-- Favicons -->
+  <link href="assets/depan/img/favicon.png" rel="icon">
+  <link href="assets/depan/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/depan/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/depan/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/depan/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/depan/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/depan/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/depan/css/main.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Personal
+  * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
+  * Updated: Aug 07 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body class="index-page">
+
+  <header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+      <a href="index.php" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1 class="sitename">Personal</h1>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="index.php" class="active">Home</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="resume.php">Resume</a></li>
+          <li><a href="services.php">Services</a></li>
+          <li><a href="portofolio.php">Portofolio</a></li>
+          <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="#">Dropdown 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="#">Deep Dropdown 1</a></li>
+                  <li><a href="#">Deep Dropdown 2</a></li>
+                  <li><a href="#">Deep Dropdown 3</a></li>
+                  <li><a href="#">Deep Dropdown 4</a></li>
+                  <li><a href="#">Deep Dropdown 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Dropdown 2</a></li>
+              <li><a href="#">Dropdown 3</a></li>
+              <li><a href="#">Dropdown 4</a></li>
+            </ul>
+          </li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+    </div>
+  </header>
+
+  <main class="main">
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section dark-background">
+
+    <img src="admin/upload/<?php echo $rowKonten['foto'] ?>" alt="" data-aos="fade-in">
+
+      <div class="container" data-aos="zoom-out" data-aos-delay="100">
+        <!-- BUAT NAMBAH NAMA USER -->
+        <h2><?php echo $rowKonten['nama_user'] ?></h2>
+        <!-- BUAT NAMBAH KETERANGAN KONTEN -->
+        <p><?php echo $rowKonten['keterangan'] ?></p>
+        <div class="social-links">
+          <a href="#"><i class="bi bi-twitter-x"></i></a>
+          <a href="#"><i class="bi bi-facebook"></i></a>
+          <a href="https://www.instagram.com/nasywaasyifa"><i class="bi bi-instagram"></i></a>
+          <a href="#"><i class="bi bi-linkedin"></i></a>
+        </div>
+      </div>
+
+    </section><!-- /Hero Section -->
+
+  </main>
+
+  <footer id="footer" class="footer dark-background">
+    <div class="container">
+      <h3 class="sitename">Personal</h3>
+      <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+      <div class="social-links d-flex justify-content-center">
+        <a href=""><i class="bi bi-twitter-x"></i></a>
+        <a href=""><i class="bi bi-facebook"></i></a>
+        <a href="https://www.instagram.com/nasywaasyifa"><i class="bi bi-instagram"></i></a>
+        <a href=""><i class="bi bi-skype"></i></a>
+        <a href=""><i class="bi bi-linkedin"></i></a>
+      </div>
+      <div class="container">
+        <div class="copyright">
+          <span>Copyright</span> <strong class="px-1 sitename">Personal</strong> <span>All Rights Reserved</span>
+        </div>
+        <div class="credits">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you've purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
+          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/depan/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/depan/vendor/php-email-form/validate.js"></script>
+  <script src="assets/depan/vendor/aos/aos.js"></script>
+  <script src="assets/depan/vendor/typed.js/typed.umd.js"></script>
+  <script src="assets/depan/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/depan/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/depan/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/depan/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/depan/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/depan/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/depan/js/main.js"></script>
+
+</body>
+
+</html>
